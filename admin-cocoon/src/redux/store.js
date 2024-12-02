5000cc6fe5d9c6f9a6a77f406d89746abf23bfa5
DucHuy267 <155927918+DucHuy267@users.noreply.github.com>
2024-12-02 14:26:23 +0700

    Delete admin-cocoon directory

--- admin-cocoon/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../redux/slides/counterSlice'
-
-export const store = configureStore({
-    reducer: {
-        counter: counterReducer,
-    },
-})
