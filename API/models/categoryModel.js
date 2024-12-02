8da364052a97e256374e9123724862dbae95f656
DucHuy267 <155927918+DucHuy267@users.noreply.github.com>
2024-12-02 14:26:01 +0700

    Delete API directory

--- API/models/categoryModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const categorySchema = new mongoose.Schema({
-    categoryId: { type: String, required: true, unique: true, },
-    categoryName: { type: String, required: true, },
-    image: { type: String, required: false, }
-});
-
-module.exports = mongoose.model('categories', categorySchema);
